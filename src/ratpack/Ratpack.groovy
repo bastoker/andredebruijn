53b20146f7c4091e341b71a0fe7fd154f68a30a6
Bas Stoker <Bas Stoker>
2015-12-29 00:41:29 +0100

    Initial version based on Ratpack 1.1.1, groovy with handlebars

---
+++ src/ratpack/Ratpack.groovy
@@ -0,0 +1,22 @@
+import ratpack.handlebars.HandlebarsModule
+
+import static ratpack.groovy.Groovy.ratpack
+import static ratpack.handlebars.Template.handlebarsTemplate
+
+ratpack {
+  bindings {
+    module HandlebarsModule
+  }
+
+  handlers {
+  	get () {
+  		render handlebarsTemplate("index.html", null, "text/html; charset=utf-8 ")	
+  	}
+
+  	get (':name.html') {
+		render handlebarsTemplate(context.pathTokens['name'] + ".html", null, "text/html; charset=utf-8 ")
+  	}
+    
+    files { dir "public" }
+  }
+}
